6ecc7abbab56ac35aeedbbee8c7d1b9f0868d6df
yakovliam <jacob@yakovliam.com>
2023-06-13 16:16:36 -0400

    feat: add cursor types

---
+++ src/utils/types/cursor/cursor-state.ts
@@ -0,0 +1,7 @@
+type CursorState = {
+  id: string; // id of the cursor
+  x: number | null; // x coordinate of the cursor
+  color: string; // color of the cursor
+};
+
+export default CursorState;
