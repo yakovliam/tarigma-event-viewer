b37cfa21b53bc84a738760b553cce2da72dfff0e
yakovliam <jacob@yakovliam.com>
2023-06-14 19:01:52 -0400

    feat: add primitive event logic

--- src/app/components/panes/events/EventsPane.tsx
+++ src/app/components/panes/events/EventsPane.tsx
@@ -1,9 +1,22 @@
-import { useRecoilValue } from "recoil";
+import { useRecoilState, useRecoilValue } from "recoil";
 import { blueprintThemeRepository } from "../../../../utils/recoil/atoms";
 import { isDarkTheme } from "../../../../types/blueprint/theme-utils";
 import PaneWrapper from "../PaneWrapper";
-import { Tag, Text } from "@blueprintjs/core";
+import {
+  Alignment,
+  Button,
+  Card,
+  Classes,
+  Collapse,
+  Elevation,
+  Navbar,
+  Tag,
+  Text,
+} from "@blueprintjs/core";
 import { styled } from "styled-components";
+import { eventsState as eventsStateAtom } from "../../../../utils/recoil/atoms";
+import Comtrade from "../../../../types/data/comtrade/comtrade";
+import { useEffect, useState } from "react";
 
 interface EventsPaneProps {
   viewId: string;
@@ -20,13 +33,161 @@ const TemporaryTextWrapper = styled(Text)`
   justify-content: center;
 `;
 
+const EventsSettingsBar = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  width: 100%;
+  height: 100%;
+`;
+
+const EventsListWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  width: 100%;
+  height: 100%;
+
+  overflow-y: auto;
+`;
+
+const EventControlsWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-grow: 1;
+  width: 100%;
+  height: 100%;
+  justify-content: space-between;
+  align-items: center;
+
+  padding: 10px;
+`;
+
+const EventControls = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const EventControl = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+type AccordianState = {
+  eventId: string;
+  isOpen: boolean;
+};
+
 const EventsPane = (props: EventsPaneProps) => {
+  const [eventsState, setEventsState] = useRecoilState(eventsStateAtom);
   const blueprintTheme = useRecoilValue<string>(blueprintThemeRepository);
+
+  const [accordionState, setAccordionState] = useState<AccordianState[]>([]);
+
+  useEffect(() => {
+    setAccordionState(
+      eventsState.map((eventState) => {
+        return {
+          eventId: eventState.id,
+          isOpen: false,
+        };
+      })
+    );
+  }, [eventsState]);
+
+  const addEvent = () => {
+    setEventsState((oldEventsState: Comtrade[]) => {
+      return [
+        ...oldEventsState,
+        {
+          id: "event-" + (oldEventsState.length + 1),
+          eventId: oldEventsState.length + 1,
+        } as Comtrade,
+      ];
+    });
+  };
+
   return (
     <PaneWrapper $isDark={isDarkTheme(blueprintTheme)}>
-      <TemporaryTextWrapper>
-        Events Pane <Tag>Implementation</Tag>
-      </TemporaryTextWrapper>
+      <EventsSettingsBar>
+        <Navbar>
+          <Navbar.Group align={Alignment.LEFT}>
+            <Navbar.Heading>Manage Events</Navbar.Heading>
+            <Navbar.Divider />
+            <Tag>{eventsState.length} loaded</Tag>
+          </Navbar.Group>
+          <Navbar.Group align={Alignment.RIGHT}>
+            <Button
+              className={Classes.MINIMAL}
+              icon="add"
+              text="Add Event"
+              onClick={addEvent}
+            />
+          </Navbar.Group>
+        </Navbar>
+        <EventsListWrapper>
+          {eventsState.map((event, index) => {
+            return (
+              <div>
+                <Card style={{ padding: 0 }}>
+                  <EventControlsWrapper>
+                    <Text>
+                      Event <Tag minimal round>{event.eventId}</Tag>
+                    </Text>
+                    <EventControls>
+                      <EventControl>
+                        <Button
+                          minimal
+                          icon="remove"
+                          onClick={() => {
+                            setEventsState((oldEventsState) => {
+                              return oldEventsState.filter(
+                                (eventState) => eventState.id !== event.id
+                              );
+                            });
+                          }}
+                        />
+                      </EventControl>
+                      <EventControl>
+                        <Button
+                          minimal
+                          rightIcon={
+                            accordionState[index]?.isOpen
+                              ? "caret-up"
+                              : "caret-down"
+                          }
+                          onClick={() => {
+                            setAccordionState((oldAccordionState) => {
+                              return oldAccordionState.map((accordionState) => {
+                                if (accordionState.eventId === event.id) {
+                                  return {
+                                    ...accordionState,
+                                    isOpen: !accordionState.isOpen,
+                                  };
+                                } else {
+                                  return accordionState;
+                                }
+                              });
+                            });
+                          }}
+                        />
+                      </EventControl>
+                    </EventControls>
+                  </EventControlsWrapper>
+                </Card>
+                <Collapse isOpen={accordionState[index]?.isOpen}>
+                  <Card>
+                    <TemporaryTextWrapper>
+                      <Text>Event Settings</Text>
+                    </TemporaryTextWrapper>
+                  </Card>
+                </Collapse>
+              </div>
+            );
+          })}
+        </EventsListWrapper>
+      </EventsSettingsBar>
     </PaneWrapper>
   );
 };
