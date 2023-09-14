1e4b273862eec247bb9c4040b1703d914504de86
TikoTheArmenian <40445154+TikoTheArmenian@users.noreply.github.com>
2023-09-14 17:13:55 -0400

    dasd

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/tarigma-event-viewer-gh-pages/',
   build: { //add this property
     sourcemap: true,
 },
